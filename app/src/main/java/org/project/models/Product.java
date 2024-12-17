6a6593301078e66722d93d88d7a3544e35173f52
Murat Alperen Ulutaş <murat.alperenulutas@hotmail.com>
2024-12-17 23:27:37 +0300

    Add Product model

---
+++ app/src/main/java/org/project/models/Product.java
@@ -0,0 +1,67 @@
+package org.project.models;
+
+public class Product {
+    public Integer id;
+    public String name;
+    public String description;
+    public String sellerId;
+    public String imageUrl;
+    public Double price;
+
+    public Product(Integer id, String name, String description, String sellerId, String imageUrl, Double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.sellerId = sellerId;
+        this.imageUrl = imageUrl;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(String sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
