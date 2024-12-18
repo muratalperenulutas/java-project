cd6709ac69425fdb20c50e54d1b9af5dc8abcf0e
Murat Alperen Ulutaş <murat.alperenulutas@hotmail.com>
2024-12-18 18:18:55 +0300

    Add UserService

---
+++ app/src/main/java/org/project/services/UserService.java
@@ -0,0 +1,53 @@
+package org.project.services;
+
+import org.project.data.JsonRepository;
+import org.project.models.User;
+
+import java.util.List;
+
+public class UserService {
+    private static JsonRepository<User> userRepository;
+
+    public UserService(JsonRepository<User> userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public static void addUser(User user) {
+        User user4=getUserByEmail(user.getEmail());
+        if(user4 != null) {
+            throw new IllegalArgumentException("User already exists");
+        }
+        userRepository.save(user);
+    }
+
+    public static void updateUser(User user) {
+        userRepository.update(user);
+    }
+
+    public static void deleteUser(Integer userId) {
+        userRepository.delete(userId);
+    }
+
+    public static List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public static User authenticate(String email, String password) {
+        List<User> users = userRepository.findAll();
+        for (User user : users) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+        return null;
+    }
+    public static User getUserByEmail(String email) {
+        List<User> users = userRepository.findAll();
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                return user;
+            }
+        }
+        return null;
+    }
+}
